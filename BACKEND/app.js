b5969f66b3fb44459c7422babe25f2c3f2ee9063
Vishal Kumar Singh <vishal091200kumar@gmail.com>
2023-05-14 11:18:23 +0530

    Update app.js

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -48,7 +48,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "", // enter mongodb connection string
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hzjkr8v.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`, // enter mongodb connection string
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
 
